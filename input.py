77b0059ef215070237810302507dff3a4500b7c6
Wellington <148399848+wellington4220@users.noreply.github.com>
2025-04-01 18:48:46 -0300

    Update input.py

--- input.py
+++ input.py
@@ -1,5 +1,5 @@
 nome = input("insira seu nome aqui: ")
 idade = input("insira aqui sua idade: ")
-peso = input("insira aqui seu peso: ")
+peso = input("insira aqui seu peso:  ")
 
 print(nome,idade,peso)
